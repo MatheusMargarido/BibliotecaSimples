4d508f6291f79fe944cc8f2a09872d1bfb468ef0
MatheusMargarido <matheusmargarido.contato@gmail.com>
2025-05-10 17:04:09 -0300

    feat: implementando funcionalidade inicial de adicionar e listar livros

---
+++ src/entities/Biblioteca.java
@@ -0,0 +1,16 @@
+package entities;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+	private List<Livro> livros = new ArrayList<>();	
+	
+	public void adicionarLivro(String titulo, String autor) {
+		livros.add(new Livro(titulo, autor));
+	}
+	
+	public List<Livro> listaLivros() {
+		return new ArrayList<>(livros);
+	}
+	
+}
